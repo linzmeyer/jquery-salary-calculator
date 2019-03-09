940ada2969f3ebd8966f8a1c6f5628ff3709a0c1
chase <chaselinzmeyer@gmail.com>
2019-03-09 13:19:19 -0600

    added feature: Employee class, getTotalMonthly(), tested with chase obj

--- scripts/jq-salary-calc.js
+++ scripts/jq-salary-calc.js
@@ -1,6 +1,21 @@
 // Globals
 
+// class to create all Employee instances
+class Employee {
+  constructor( firstName, lastName, id, title, annualSalary ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+    this.title = title;
+    this.annualSalary = annualSalary;
+  }
+}
 
+// array to hold all employee objects
+let allEmployees = [];
+
+// Variable to hold totalMonthly salaries for all Employees
+let totalMonthly = 0;
 
 /*******************************************************************************
 *** Application Start **********************************************************
@@ -10,6 +25,11 @@
 $( document ).ready( readyNow );
 
 
+let chase = new Employee( 'Chase', 'Linzmeyer', 1234, 'Web Developer', 80000 );
+
+allEmployees.push( chase );
+
+totalMonthly = getTotalMonthly( allEmployees );
 
 
 
@@ -23,16 +43,15 @@ $( document ).ready( readyNow );
 
 // DOM initialization, event listeners
 function readyNow() {
+
   console.log( 'in readyNow' );
+
+
 
   //----------------------------------------------------------------------------
   // DOM INIT ------------------------------------------------------------------
   //----------------------------------------------------------------------------
-  $(document).ready(function(){
-        $(".add-row").click(function(){
-            var name = $("#name").val();
-            var email = $("#email").val();
-            var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + name + "</td><td>" + email + "</td></tr>";
+
 
   //----------------------------------------------------------------------------
   // EVENT LISTENERS -----------------------------------------------------------
@@ -53,4 +72,25 @@ function readyNow() {
 //------------------------------------------------------------------------------
 // OTHER FUNCTIONS -------------------------------------------------------------
 //------------------------------------------------------------------------------
+function getTotalMonthly( arrEmplyees ) {
+  console.log( 'in getTotalMonthly' );
+
+  let total = 0;
+
+  for ( let employee of arrEmplyees ) {
+    total += employee.annualSalary;
+  }
+
+  let totalYear = total;
+
+  total /= 12;
+
+  console.log( 'total Year', totalYear );
+  console.log( 'total Monthly', totalMonthly );
+
+  return total;
+}
+
+
+
 
